df691d2376db6c15b650043d6880484d048de6e7
ilovezfs <ilovezfs@icloud.com>
2016-09-01 23:40:46 -0700

    django-completion 1.10.1
    Closes #137.
    
    Signed-off-by: ilovezfs <ilovezfs@icloud.com>

--- django-completion.rb
+++ django-completion.rb
@@ -1,8 +1,8 @@
 class DjangoCompletion < Formula
   desc "Bash completion for Django"
   homepage "https://github.com/django/django"
-  url "https://github.com/django/django/archive/1.10.tar.gz"
-  sha256 "db122380ae02e99a72aaafc92ef92b9eb5a5b0371a5722f96ccae805d4d2cb59"
+  url "https://github.com/django/django/archive/1.10.1.tar.gz"
+  sha256 "82a085c3bdb6e88a169dc77258b00729cc3e69f802704e8d600128cd992a2d17"
   head "https://github.com/django/django.git"
 
   bottle :unneeded
